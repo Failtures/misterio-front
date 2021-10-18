2f8b250407fa0c8cb1f130061eaec974ffa30267
AleSpitale <alespitale6@gmail.com>
2021-10-18 11:39:47 -0300

    Button join game better

--- src/components/Button2.jsx
+++ src/components/Button2.jsx
@@ -3,11 +3,10 @@ import React from "react"
 const Button1 = () => {
   return (
     <div>
-      <button onClick> Create game </button>
+      <button onClick> Join Game </button>
     </div>
   );
 }
 // FIXME onClick of button.
 // NOTE Make the conection of the button to the corresponding function
-
 export default Button1
