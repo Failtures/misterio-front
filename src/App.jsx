0dbcf4551c2f93cc2ffb7452c0bea764af785a1b
AleSpitale <alespitale6@gmail.com>
2021-10-19 11:34:45 -0300

    buttonCreateGame plus clean repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import ButtonCreateGame from './components/ButtonCreateGame';
+
+function App() {
+  return (
+    <div>
+      <ButtonCreateGame></ButtonCreateGame>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ButtonCreateGame.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const ButtonCreateGame = () => {
+  return (
+    <div>
+      <button onClick = {openModal}> Create Game </button>
+    </div>
+  );
+}
+// FIXME onClick of button.
+// NOTE Make the conection of the button to the corresponding function
+export default ButtonCreateGame
