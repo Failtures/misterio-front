3e2c3414335f6e1dd243c17c0b7f9eae7b55c2e4
AleSpitale <alespitale6@gmail.com>
2021-10-24 18:08:14 -0300

    Button Join Game tested

--- src/components/Buttons/ButtonJoinGame.jsx
+++ src/components/Buttons/ButtonJoinGame.jsx
@@ -8,76 +8,30 @@ import { useHistory } from "react-router";
 const ButtonJoinGame = (props) => {
 
   const history = useHistory()
-  const [response, setResponse] = useState();
-
-  const joinGame = async () => {
-
-    try {
-
-      const response = await axios({
-        method: 'put',
-        url: `${api.url}/join-player?name=${props.nameGame}&player=${props.player}`,
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      });
-
-      const data = response.data;
-      console.log(data)
-      console.log(data.lobbies)
-
-
-
-
-
-      /*
-      if (data.lobbies != null) {
-        history.push('/Prueba');
-      }
-      else {
-        alert(`${data.info}`);
-      }
-      */
-    }
-
-    catch (error) {
-
-      console.log(error, "The lobby is full or the player is already in the lobby");
-
-    }
-
-  };
 
   return (
     <div>
       <Button variant="contained" color="secondary" href="" onClick={async () => {
-        
+
         try {
 
           const response = await axios({
-            method: 'put',
+            method: 'post',
             url: `${api.url}/join-player?name=${props.nameGame}&player=${props.player}`,
-
-              /*'https://misterio-famaf.herokuapp.com/join-player?name=lobby%20perro&player=ale'*/
             headers: {
-              'Content-Type': 'multipart/form-data'
+              'Content-Type': 'multipart/form-data',
             }
           });
 
-          const data = response.data;
-          console.log(data)
-          console.log(data.lobbies)
-          
-
+          history.push('/')
         }
 
         catch (error) {
-
-          console.log(error, "The lobby is full or the player is already in the lobby asdsadsadasdas");
-          console.log(JSON.stringify(error));
-
+          console.log(error, "The lobby is full or the player is already in the lobby");
         }
-      }}> Join Game </Button>
+
+      }
+      }> Join Game </Button>
     </div>
   );
 };
