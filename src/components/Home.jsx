05ee5f02d85ff00967762a1312cbc17586418490
israel2021 <israelangulo2021@gmail.com>
2021-10-26 18:14:12 -0300

    estilos en home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,18 +7,23 @@ import ButtonCreateGame from './Buttons/ButtonCreateGame';
 import ListGames from './ListGames';
 import ButtonThrowDice from './Buttons/ButtonThrowDice';
 import ThrowDice from './Buttons/ButtonThrowDice';
+import { color } from '@mui/system';
 
 const Home = () => {
+
     const [nickName, setNickName] = useState('');
     const [isOpenModal, openModal, closeModal] = useModal(false);
+
     return (
         <div className="container">
+            <h1 style={{color:'white'}}>MISTERIO</h1>
             <div className="section">
-                <ButtonCreateGame openModal={openModal}/>
-                <ListGames player={nickName}></ListGames> 
+                <TextField id="outlined-basic" label="Nickname" variant="outlined" onChange={(e) => { setNickName(e.target.value) }} />
+                <ButtonCreateGame openModal={openModal} />
+                <ListGames player={nickName}></ListGames>
                 <Modal isOpen={isOpenModal} closeModal={closeModal} player={nickName} />
             </div>
-            <TextField id="outlined-basic" label="Nickname" variant="outlined" onChange={(e) => { setNickName(e.target.value) }} />
+
         </div>
     )
 }
