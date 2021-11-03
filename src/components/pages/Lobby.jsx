3d56d5c850895714d06df4f1f086e6d647d180a2
AleSpitale <alespitale6@gmail.com>
2021-11-03 17:36:10 -0300

    Throw Dice Fixed

--- src/components/pages/Lobby.jsx
+++ src/components/pages/Lobby.jsx
@@ -61,7 +61,7 @@ const Lobby = () => {
     return (
 
         <div>
-            <h1>Lobby</h1>
+            <h2>Lobby</h2>
             <ul>
                 {
                     players.map(item => (
