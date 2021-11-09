1520cb71e2ef2405f54307c15aa8d9204e7ad875
israel2021 <israelangulo2021@gmail.com>
2021-11-08 23:54:41 -0300

    Fix Lobbby working

--- src/components/pages/Lobby.jsx
+++ src/components/pages/Lobby.jsx
@@ -10,24 +10,25 @@ import { Alert } from "@material-ui/core";
 
 const Lobby = () => {
 
-    const [host, setHost] = useState('');
     const [newplayer, setNewPlayer] = useState('')
 
     const colors_token = ['green', 'blue', 'red', 'yellow', 'pink', 'orange']
 
-    const dictStates = useContext(ThemeContext)
+    const dictStates = useContext(ThemeContext);
 
     const history = useHistory();
 
     const alertRef = useRef(null);
+
+    const [host, setHost] = useState('')
+
+    const [players2, setPlayers2] = useState([]);
 
     const takesGetHand = {
         'action': 'match_get_hand',
         'player_name': dictStates.nickname,
         'match_name': dictStates.lobbyName
     };
-
-    let arrayAuxiliar = [];
 
     useEffect(() => {
 
@@ -36,22 +37,19 @@ const Lobby = () => {
             const parseJson = JSON.parse(e.data);
 
             if (parseJson.action === 'new_lobby') {
-                dictStates.setLobbyName(parseJson.lobby.name)
                 setHost(parseJson.lobby.host);
+                setPlayers2([parseJson.lobby.host]);
                 dictStates.setPlayers(parseJson.lobby.players);
+                dictStates.setLobbyName(parseJson.lobby.name);
             }
             else if (parseJson.action === 'joined_lobby') {
-                dictStates.setLobbyName(parseJson.lobby.name)
+                setPlayers2(parseJson.lobby.players);
                 dictStates.setPlayers(parseJson.lobby.players);
+                dictStates.setLobbyName(parseJson.lobby.name);
             }
             else if (parseJson.action === 'new_player') {
-                arrayAuxiliar = dictStates.players.slice();
-                arrayAuxiliar.push(parseJson.player_name);
-                dictStates.setPlayers(arrayAuxiliar);
-                setNewPlayer(parseJson.player_name);
+                setPlayers2((players2) => [...players2, parseJson.player_name]);
                 alertRef.current.style.display = 'block';
-
-                dictStates.setPlayers(arrayAuxiliar);
             }
             else if (parseJson.action === 'match_started') {
                 ws.send(JSON.stringify(takesGetHand));
@@ -75,7 +73,7 @@ const Lobby = () => {
                 history.push(`/game/${parseJson.match.name}`);
             }
             else if (parseJson.action === 'player_left') {
-
+                dictStates.setPlayers()
             }
             else if (parseJson.action === 'lobby_removed') {
                 history.push('/');
@@ -83,20 +81,15 @@ const Lobby = () => {
         };
     });
 
-    console.log(`turno inicial: ${dictStates.turn}`);
-    console.log(`color del jugador: ${dictStates.tokenColor}`);
-    console.log(`posicion X inicial: ${dictStates.posX}`);
-    console.log(`posicion Y inicial: ${dictStates.posY}`);
-
     return (
 
         <div>
             <h2>Lobby</h2>
             <ul>
                 {
-                    dictStates.players.map(item => (
-                        <li> {item} </li>
-                    ))
+
+                    players2.map(player => <li>{player}</li>)
+
                 }
             </ul>
 
@@ -111,6 +104,9 @@ const Lobby = () => {
             <Alert style={{ display: 'none' }} ref={alertRef} onClose={() => { alertRef.current.style.display = "none" }} variant="filled" severity="info">
                 A new player has joined:{newplayer}
             </Alert>
+
+
+
 
         </div>
     );
