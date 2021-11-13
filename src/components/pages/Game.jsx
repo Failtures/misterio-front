6ebe1719f0fe18870ca257ae451d1d2b04fc3f47
israel2021 <israelangulo2021@gmail.com>
2021-11-13 17:34:01 -0300

    Suspect on progress

--- src/components/pages/Game.jsx
+++ src/components/pages/Game.jsx
@@ -9,6 +9,7 @@ import ModalWichCardAccuse from "../modals/ModalWichCardAccuse";
 import ModalWinOrLost from "../modals/ModalWinOrLost";
 import MchooseCardsSuspect from "../modals/MchooseCardsSuspect";
 import ModalSalem from "../modals/ModalSalem";
+import ModalSuspect from "../modals/ModalSuspect";
 
 import ButtonAccuse from "../buttons/ButtonAccuse";
 import ButtonThrowDice from "../buttons/ButtonThrowDice";
@@ -22,13 +23,14 @@ import './Lobby.css'
 
 
 const Game = () => {
-    
+
     const dictStates = useContext(ThemeContext);
 
     const [isOpenAccuse, openModalAccuse, closeModalAccuse] = useModal(false);
     const [isOpenSuspect, openModalSuspect, closeModalSuspect] = useModal(false);
     const [isOpenWinOrLost, openModalWinOrLost, closeModalWinOrLost] = useModal(false);
     const [isOpenSalem, openModalSalem, closeModalSalem] = useModal(false);
+    const [isOpenQuestion, openModalQuestion, closeModalQuestion] = useModal(false);
 
     const [dice, setDice] = useState(0);
     const [diceRolled, setDiceRolled] = useState(false);
@@ -37,6 +39,8 @@ const Game = () => {
     const [hand, setHand] = useState([]);
     const [salem, setSalem] = useState({});
     const [mistery, setMistery] = useState('');
+    const [suspect, setSuspect] = useState([]);
+    const [replyTo, setReplyTo] = useState('');
 
     const refButtonMistery = useRef(null);
 
@@ -59,7 +63,45 @@ const Game = () => {
                 };
             }
             else if (parsedJson.action === 'question') {
-                console.log(parsedJson);
+
+                console.log(`Jugador que sospecho ${parsedJson.reply_to}`);
+
+                setReplyTo(parsedJson.reply_to);
+
+                const suspectCards = hand.filter(
+                    card =>
+                        card.name === parsedJson.monster ||
+                        card.name === parsedJson.victim ||
+                        card.name === parsedJson.room
+                );
+
+                console.log(`Cartas a mostrar ${suspectCards}`);
+                console.log(`Cartas del jugador: ${hand}`);
+
+                if (suspectCards.length > 0) {
+                    console.log('dentro del if')
+                    setSuspect(suspectCards);
+                    openModalQuestion();
+                }
+                else {
+                    console.log('dentro del else')
+                    const takesQuestionNegative = {
+                        'action': 'match_question_res',
+                        'response': 'negative',
+                        'player_name': dictStates.nickname,
+                        'reply_to': replyTo,
+                        'match_name': dictStates.lobbyName,
+                        'reply_card': '',
+                        'monster': parsedJson.monster,
+                        'victim': parsedJson.victim,
+                        'room': parsedJson.room
+                    };
+
+                    ws.send(JSON.stringify(takesQuestionNegative));
+                }
+            }
+            else if (parsedJson.action === 'suspect_response') {
+                console.log(parsedJson.card);
             }
             else if (parsedJson.action === 'game_over') {
                 setWinner(parsedJson.winner);
@@ -115,7 +157,14 @@ const Game = () => {
                 loser={loser}
                 winner={winner}
             />
-            
+
+            <ModalSuspect
+                isOpenQuestion={isOpenQuestion}
+                closeModalQuestion={closeModalQuestion}
+                suspect={suspect}
+                replyTo={replyTo}
+            />
+
             {salem && <Button ref={refButtonMistery} variant="contained"
                 color="secondary" onClick={openModalSalem}>Use Salem</Button>}
             {mistery && <p>{`Mistery card: ${mistery}`}</p>}
